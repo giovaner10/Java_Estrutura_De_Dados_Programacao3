3c1ac378d4189f546944359f9606559fa9b6040f
giovaner10 <giovanerr10@gmail.com>
2022-04-18 12:37:17 -0300

    RunTimeException que lança excessao caso um elemento seja inserido em uma posicao invalida

---
+++ src/br/com/cesed/p3/excessoes/PosicaoInvalidaException.java
@@ -0,0 +1,8 @@
+package br.com.cesed.p3.excessoes;
+
+public class PosicaoInvalidaException extends RuntimeException{
+
+    public PosicaoInvalidaException(String message) {
+        super(message);
+    }
+}
