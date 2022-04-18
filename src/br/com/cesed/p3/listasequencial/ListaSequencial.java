711fd7c4cba06a57715342829114c3c55c3dc04b
giovaner10 <giovanerr10@gmail.com>
2022-04-18 13:03:19 -0300

    ListaSequencial.java

--- src/br/com/cesed/p3/listasequencial/ListaSequencial.java
+++ src/br/com/cesed/p3/listasequencial/ListaSequencial.java
@@ -10,7 +10,11 @@ public class ListaSequencial {
 
     private int inseridos;
 
-
+    /**
+     * Metodo que serve para adicionar um novo elemento.
+     *
+     * @param novoElemento Elemento que vai ser adicionado.
+     */
     public void add(Object novoElemento){
         if(novoElemento == null){
             throw new ValorInvalidoException("Elemento nulo nao pode ser inserido");
@@ -22,11 +26,20 @@ public class ListaSequencial {
         inseridos++;
     }
 
+    /**
+     *
+     * @return O tamanho da lista.
+     */
     public int size(){
         return inseridos;
 
     }
 
+    /**
+     *
+     * @param index Busca um objeto na lista com esse index.
+     * @return Retorna o objeto com o index.
+     */
     public Object get(int index){
         if (index < 0 || index > arrayInterno.length){
             throw new PosicaoInvalidaException("Impossivel inseeri na posicao: " + index);
@@ -35,17 +48,28 @@ public class ListaSequencial {
         return arrayInterno[index];
     }
 
+    /**
+     * Limpa a lista.
+     */
     public void clear(){
         inseridos = 0;
         arrayInterno = new Object[ALOCACAO_INICIAL];
 
     }
 
+    /**
+     *
+     * @return Retorna se a lista esta vazia ou não.
+     */
     public boolean isEmpty(){
         return inseridos == 0;
     }
 
-    public Object remove(Object elemento){
+    /**
+     *
+     * @return Remove o ultimo o elemento e o retorna.
+     */
+    public Object remove(){
         Object objectRemovido = arrayInterno[inseridos - 1];
         arrayInterno[inseridos - 1] = null;
         inseridos--;
