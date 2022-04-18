b0cff6a598092a946485e158e4ec98d6be88f5f8
giovaner10 <giovanerr10@gmail.com>
2022-04-18 12:27:24 -0300

    RunTimeExcpetion no caso de adicionar um valor invalido na estrutura

---
+++ src/br/com/cesed/p3/excessoes/ValorInvalidoException.java
@@ -0,0 +1,7 @@
+package br.com.cesed.p3.excessoes;
+
+public class ValorInvalidoException extends RuntimeException{
+    public ValorInvalidoException(String message) {
+        super(message);
+    }
+}
